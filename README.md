cf0fc378e35a626984e03cea8ff89da6bf1baca6
stefanselic <s.stefan22@hotmail.com>
2023-05-08 00:14:33 +0200

    Add components, readme

--- README.md
+++ README.md
@@ -4,22 +4,22 @@ Create a web app with React that allows for users to generate and download memes
 
 It should allow the user to:
 
-- [ ] Enter text for the top and bottom of the meme
-  - [ ] The top text box needs to have a label element associated with it containing the text `Top text`
-  - [ ] The bottom text box needs to have a label element associated with it with the text `Bottom text`
-  - [ ] Both text boxes should be empty when the page first loads
-- [ ] Preview the generated meme
-  - [ ] The image element needs to have an html attribute set as follows: `data-test-id="meme-image"`
-    - [ ] This image element should show a working image when the page first loads
-- [ ] Change the meme template (the background image)
-  - [ ] The meme template selector element needs to have a label element associated with it containing the text `Meme template`
-  - [ ] If the user follows the steps below, the `doge` meme template needs to be selected:
+- [x] Enter text for the top and bottom of the meme
+  - [x] The top text box needs to have a label element associated with it containing the text `Top text`
+  - [x] The bottom text box needs to have a label element associated with it with the text `Bottom text`
+  - [x] Both text boxes should be empty when the page first loads
+- [x] Preview the generated meme
+  - [x] The image element needs to have an html attribute set as follows: `data-test-id="meme-image"`
+    - [x] This image element should show a working image when the page first loads
+- [x] Change the meme template (the background image)
+  - [x] The meme template selector element needs to have a label element associated with it containing the text `Meme template`
+  - [x] If the user follows the steps below, the `doge` meme template needs to be selected:
     1. Click on the label of the meme template selector
     2. Clear any existing value (eg. with a text box)
     3. Type the text `doge`
     4. Hit enter
 - [ ] Download the meme by clicking on a button
-  - [ ] The button element needs to contain the text `Download`
+  - [x] The button element needs to contain the text `Download`
 
 # Getting Started with Create React App
 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import './App.css';
-import logo from './logo.svg';
+import styles from './App.css';
+import { useState } from 'react';
+import { saveAs } from 'file-saver';
+import Example from './components/Select.js';
+const baseUrl =
+  'https://api.memegen.link/images/buzz/top/bottom.gif?token=wxgjeu3jll4dt9q6fihy&amp;width=800&amp;frames=50';
 
 export default function App() {
+  // These state variables will be used to store the text input by the user for the top and bottom of the meme and the selected template for the meme.
+  const [topInputText, setTopInputText] = useState('');
+  const [bottomInputText, setBottomInputText] = useState('');
+  const [memeTemplate, setMemeTemplate] = useState('');
+  const [src, setSrc] = useState(baseUrl);
+
+  // Prevents the event to do whatever it is doing and now we can modify our src code with template literals
+  const handleDownload = (event) => {
+    event.preventDefault();
+    setSrc(
+      `https://api.memegen.link/images/${memeTemplate}/${topInputText}/${bottomInputText}.gif?token=wxgjeu3jll4dt9q6fihy&amp;width=800&amp;frames=50`,
+    );
+    const currentSrc = saveAs(src);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="App">
+        <form>
+          <div>
+            <label htmlFor="top-text">Top text:</label>
+            <input
+              value={topInputText}
+              onChange={(event) => setTopInputText(event.currentTarget.value)}
+            />
+          </div>
+          <div>
+            <Example />
+            <label htmlFor="meme-template">Meme template:</label>
+            <input
+              value={memeTemplate}
+              onChange={(event) => setMemeTemplate(event.currentTarget.value)}
+            />
+          </div>
+          <img data-test-id="meme-image" src={src} alt="Generated Meme" />
+          <div>
+            <label htmlFor="bottom-text">Bottom text:</label>
+            <input
+              value={bottomInputText}
+              onChange={(event) =>
+                setBottomInputText(event.currentTarget.value)
+              }
+            />
+          </div>
+          {/* <MyComponent /> */}
+          <button onClick={handleDownload}>Download</button>
+        </form>
+      </div>
     </div>
   );
 }
